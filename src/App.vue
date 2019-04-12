38525e16813f94f15ff61d56dc7851812702179d
Yii.Guxing <yii.guxing@gmail.com>
2019-04-12 17:19:41 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        components: {}
+    }
+</script>
+
+<style>
+    #app {
+        height: 100%;
+        font-family: 'Microsoft YaHei', 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        background-size: cover;
+        background-color: #03A9F4;
+        background-image: url("./assets/background.jpg");
+    }
+</style>
